68c211c6317a468b5d1b44a0dc6f5ce7c2562c27
valentinalazar.dev <valentinalazar.dev@gmail.com>
2021-10-25 16:04:38 +0300

    How OAuth Works
    Step 1 – The User Shows Intent
    Step 2 – The Consumer Gets Permission
    Step 3 – The User Is Redirected to the Service Provider
    Step 4 – The User Gives Permission
    Step 5 – The Consumer Obtains an Access Token
    
    OAuth 2.0 works with the following four actors:
    
    - authorization server: responsible for authentication and authorization — it provides the access token.
    - resource server: in charge of serving up resources if a valid token is provided.
    - resource owner: the owner of the data — that is, the end user of Incognito.
    - client: the Incognito mobile app.
    
    https://www.raywenderlich.com/243-oauth-2-0-with-swift-tutorial https://github.com/p2/OAuth2#usage

--- ToyApp/ViewController.swift
+++ ToyApp/ViewController.swift
@@ -6,10 +6,22 @@
 //
 
 import UIKit
+import OAuth2
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var signIn: UIButton!
+    
+    let oauth2 = OAuth2CodeGrant(settings: [
+        "client_id": "95601505476e322699cc",
+        "client_secret": "cf82ef7faab5d4d7ebe14663c51c8dcb13bf19c8",
+        "authorize_uri": "https://github.com/login/oauth/authorize",
+        "token_uri": "https://github.com/login/oauth/access_token",   // code grant only
+        "redirect_uris": ["ToyApp://oauth/callback"],   // register your own "myapp" scheme in Info.plist
+        "scope": "user repo:status",
+        "secret_in_body": true,    // Github needs this
+        "keychain": false,         // if you DON'T want keychain integration
+    ] as OAuth2JSON)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +32,13 @@ class ViewController: UIViewController {
         
         signIn.setTitle("Sign in", for: UIControl.State.normal)
         signIn.setTitle("SIGN IN", for: .highlighted)
+        oauth2.authConfig.authorizeEmbedded = true
+        oauth2.authConfig.authorizeContext =  self
     }
 
     
     
-    @IBAction func signIn(_ sender: Any) {
+    @IBAction func signIn(_ sender: Any) {        
     }
     
 }
